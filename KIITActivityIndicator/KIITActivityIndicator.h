2d6c26bbdb201d5db40db811777b41d21f137c1d
julien <julien.goudet@sweepin.fr>
2019-05-15 11:02:41 +0200

    Initial Commit

---
+++ KIITActivityIndicator/KIITActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  KIITActivityIndicator.h
+//  KIITActivityIndicator
+//
+//  Created by Julien Goudet on 15/05/2019.
+//  Copyright © 2019 Hootcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KIITActivityIndicator.
+FOUNDATION_EXPORT double KIITActivityIndicatorVersionNumber;
+
+//! Project version string for KIITActivityIndicator.
+FOUNDATION_EXPORT const unsigned char KIITActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KIITActivityIndicator/PublicHeader.h>
+
+
